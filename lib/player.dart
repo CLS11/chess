c68c4eda4601bc91dbf4f8e15b1a607760fa960c
CLS11 <crystall.101600@gmail.com>
2024-11-02 20:37:58 +0530

    Details about the characteristics of the player.

--- lib/player.dart
+++ lib/player.dart
@@ -1,3 +1,4 @@
+import 'package:chess/agents.dart';
 import 'package:flutter/material.dart';
 import 'package:chess/positions.dart';
 
@@ -5,4 +6,11 @@ class Player {
   Player({required this.position, required this.color});
   Positions position;
   final Color color;
+
+  Player move(Move move) {
+    return Player(
+      position: Positions(position.x + move.dx, position.y + move.dy),
+      color: color,
+    );
+  }
 }

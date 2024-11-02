32609df15dbb420e530fe82737a6978e0179072e
CLS11 <crystall.101600@gmail.com>
2024-11-02 20:36:11 +0530

    Added gameState to update the player's position as they move.

--- lib/board.dart
+++ lib/board.dart
@@ -1,18 +1,38 @@
+import 'package:chess/agents.dart';
 import 'package:chess/board_painter.dart';
 import 'package:chess/player.dart';
 import 'package:chess/positions.dart';
 import 'package:flutter/material.dart';
 
 class GameState {
-  final List<Player> players;
   GameState()
       : players = <Player>[
-          Player(position: Positions(0, 0), color: Colors.deepPurple),
+          Player(
+            position: Positions(0, 0),
+            color: Colors.black,
+          ),
           Player(
             position: Positions(kBoardWidth - 1, kBoardHeight - 1),
-            color: const Color.fromARGB(255, 140, 123, 189),
+            color: Colors.white,
           ),
         ];
+  final List<Player> players;
+  int currentPlayerIndex = 0;
+  int nextPlayerIndex() {
+    return (currentPlayerIndex + 1) % players.length;
+  }
+
+  Player activePlayer() {
+    return players[currentPlayerIndex];
+  }
+
+  GameState._(this.players, this.currentPlayerIndex);
+
+  GameState afterMove(Move move) {
+    var nextPlayers = List<Player>.from(players);
+    players[currentPlayerIndex] = activePlayer().move(move);
+    return GameState._(nextPlayers, nextPlayerIndex());
+  }
 }
 
 class Board extends StatelessWidget {

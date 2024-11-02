703859bd6d90e1534d4b490d3e2aab275e18a46e
CLS11 <crystall.101600@gmail.com>
2024-11-02 15:23:51 +0530

    Extending the board for the display.

---
+++ lib/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:chess/board.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  GameState gameState = GameState();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: SizedBox.expand(
+        child: Board(gameState: gameState),
+      ),
+    );
+  }
+}

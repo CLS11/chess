fb1122eccd52e03bbb612b41e2dd3436823637fc
CLS11 <crystall.101600@gmail.com>
2024-11-04 21:01:23 +0530

    Added a timer, battle start and end with next methods.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:chess/board.dart';
 import 'package:chess/game_controller.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +14,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   GameState? gameState;
   GameController gameController = GameController.demo();
+  final Map<String, int> wins = <String, int>{};
+  Timer? timer;
 
   @override
   void initState() {
@@ -20,26 +24,64 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void dispose() {
+    timer?.cancel;
+    super.dispose();
+  }
+
+  void _handleTimer(Timer _) {
+    nextTurn();
+
+    if (gameState?.isDone ?? false) {
+      var name = gameState?.winner?.name ?? "[Draw]";
+      wins[name] = (wins[name] ?? 0) + 1;
+      timer?.cancel();
+      timer = null;
+      _startBattle();
+    }
+  }
+
+  void _startBattle() {
+    gameState = gameController.getRandomInitialGameState();
+    timer = Timer.periodic(const Duration(milliseconds: 1), _handleTimer);
+  }
+
+  void _stopBattle() {
+    setState(() {
+      timer?.cancel();
+      timer = null;
+    });
+  }
+
+  void nextTurn() {
+    setState(() {
+      gameState = gameController.takeTurn(gameState!);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Align(
-        alignment: Alignment.topCenter,
-        child: AspectRatio(
-          aspectRatio: 1.0,
-          child: BoardView(gameState: gameState!),
-        ),
+      body: Row(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: AspectRatio(
+              aspectRatio: 1.0,
+              child: BoardView(gameState: gameState!),
+            ),
+          ),
+          LeaderBoard(wins: wins),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            gameState = gameController.takeTurn(gameState!);
-          });
-        },
-        backgroundColor: Colors.green,
-        child: const Icon(Icons.navigation),
+        onPressed: timer == null ? _startBattle : _stopBattle,
+        child: timer == null
+            ? const Icon(Icons.play_arrow)
+            : const Icon(Icons.stop),
       ),
     );
   }

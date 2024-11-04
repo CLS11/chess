4211e717afa24c3768dba82bc5c63bd1b5339bc7
CLS11 <crystall.101600@gmail.com>
2024-11-04 21:02:10 +0530

    Added a title.

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class Chess extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Checkers Battle',
       theme: ThemeData(),
       debugShowCheckedModeBanner: false,
       home: const HomePage(),

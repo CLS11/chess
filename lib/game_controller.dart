752984d80e7a729a0ba2b4011cfb886349836f2f
CLS11 <crystall.101600@gmail.com>
2024-11-04 17:21:10 +0530

    Sets the board and keeps the record of color of the pieces and names of players.

--- lib/game_controller.dart
+++ lib/game_controller.dart
@@ -1,21 +1,50 @@
-import 'package:chess/agents.dart';
+import 'dart:ui';
+
+import 'package:chess/agents.dart' as agents;
 import 'package:chess/board.dart';
+import 'package:chess/pieces.dart';
 import 'package:chess/player.dart';
+import 'package:chess/positions.dart';
 import 'package:flutter/material.dart';
 
-class GameController {
-  GameState gameState = GameState();
-  List<Agent> agents = <Agent>[FirstMover(), FirstMover()];
-  int currentPlayerIndex = 0;
+const List<Color> kPlayerColors = <Color>[Colors.black, Colors.white];
+const List<String> kPlayerNames = <String>["Kenny", "Bae"];
 
-  Agent activeAgent(GameState gameState) {
-    return agents[gameState.currentPlayerIndex];
+class GameController {
+  factory GameController.random(int playerCount) {
+    var controller = GameController();
+    for (var i = 0; i < playerCount; ++i) {
+      var player = Player(kPlayerNames[i], kPlayerColors[i]);
+      controller.addPlayerWithAgent(player, agents.RandomMover());
+    }
+    return controller;
+  }
+  final Map<Player, agents.Agent> _agents = <Player, agents.Agent>{};
+
+  GameController();
+
+  void addPlayerWithAgent(Player player, agents.Agent agent) {
+    _agents[player] = agent;
+  }
+
+  GameState getRandomInitialGameState() {
+    var board = Board.empty();
+    var players = _agents.keys.toList();
+    for (var player in players) {
+      Positions position;
+      do {
+        position = Positions.random();
+      } while (board.getAt(position) != null);
+
+      board = board.placeAt(position, Pieces(PieceType.king, player));
+    }
+    return GameState(board, players);
   }
 
   GameState takeTurn(GameState gameState) {
-    final agent = activeAgent(gameState);
-    final view = AgentBoardView(gameState, gameState.activePlayer());
-    final move = agent.pickMove(view);
-    return gameState.afterMove(move);
+    var activePlayer = gameState.activePlayer;
+    var view = agents.AgentView(gameState, activePlayer);
+    var activeAgent = _agents[activePlayer]!;
+    return gameState.move(activeAgent.pickMove(view));
   }
 }

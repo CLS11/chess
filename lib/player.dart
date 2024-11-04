8196465521d4e863620bf6b627ed05f635478665
CLS11 <crystall.101600@gmail.com>
2024-11-04 17:30:32 +0530

    Details about the player.

--- lib/player.dart
+++ lib/player.dart
@@ -1,16 +1,10 @@
-import 'package:chess/agents.dart';
 import 'package:flutter/material.dart';
-import 'package:chess/positions.dart';
 
 class Player {
-  Player({required this.position, required this.color});
-  Positions position;
+  Player(this.name, this.color);
+  final String name;
   final Color color;
 
-  Player move(Move move) {
-    return Player(
-      position: Positions(position.x + move.dx, position.y + move.dy),
-      color: color,
-    );
-  }
+  @override
+  String toString() => 'Player[$name]';
 }

6edf512e7698fbd45d803a8153e42e238e757914
CLS11 <crystall.101600@gmail.com>
2024-11-06 19:57:53 +0530

    Added an agent factory with Oportunists and Seeker, Runner where Agent is extendig abstract class of Player.

--- lib/agents.dart
+++ lib/agents.dart
@@ -8,6 +8,17 @@ import 'package:chess/player.dart';
 import 'package:chess/positions.dart';
 import 'package:flutter/material.dart';
 
+typedef AgentFactory = Agent Function();
+
+List<AgentFactory> all = <AgentFactory>[
+  FirstMover.new,
+  RandomMover.new,
+  Fixate.new,
+  Seeker.new,
+  Runner.new,
+  Opportunist.new,
+];
+
 class Move {
   const Move(this.initialPosition, this.finalPosition);
   final Positions initialPosition;
@@ -25,7 +36,7 @@ class Move {
         finalPosition == other.finalPosition;
   }
 
-  Delta get delta => finalPosition.deltaTo(initialPosition);
+  Delta get delta => initialPosition.deltaTo(finalPosition);
 
   @override
   int get hashCode => hashValues(initialPosition, finalPosition);
@@ -65,70 +76,134 @@ class AgentView {
   Iterable<Move> get legalMoves => _gameState.board.getLegalMoves(_player);
 }
 
-abstract class Agent {
+abstract class Agent extends Player {
   const Agent();
+
   Move pickMove(AgentView view);
 }
 
-abstract class DistanceEvaluatorAgent extends Agent {
-  bool isBetter(double currentDistance, double bestDistance);
+Move findMoveByDistanceToTarget(AgentView view, Positions targetPosition,
+    bool Function(double currentDistance, double bestDistance) isBetter) {
+  Move? bestMove;
+  double? bestDistance;
+  for (var move in view.legalMoves) {
+    var currentDistance = move.finalPosition.deltaTo(targetPosition).magnitude;
+    if (bestDistance == null || isBetter(currentDistance, bestDistance)) {
+      bestDistance = currentDistance;
+      bestMove = move;
+    }
+  }
+  return bestMove!;
+}
+
+class Seeker extends Agent {
+  Delta? target;
+
+  @override
+  String get name => 'Seeker';
+
+  @override
+  Color get color => Colors.blue;
 
   @override
   Move pickMove(AgentView view) {
-    var myKing = view.getPositions(PieceType.king).first;
-    var targetPosition = view.closestOpponent(myKing, PieceType.king);
+    var initialPosition = view.getPositions(PieceType.king).first;
+    var targetPosition = view.closestOpponent(initialPosition, PieceType.king);
     if (targetPosition == null) {
-      return view.legalMoves.first;
+      target = null;
+      return _findRandomMove(view.legalMoves);
     }
+    final move = findMoveByDistanceToTarget(view, targetPosition,
+        (double currentDistance, double bestDistance) {
+      return currentDistance < bestDistance;
+    });
+    target = move.finalPosition.deltaTo(targetPosition);
+    return move;
+  }
+}
 
-    Move? bestMove;
-    double? bestDistance;
+class Runner extends Agent {
+  @override
+  String get name => 'Runner';
 
+  @override
+  Color get color => Colors.white;
+
+  @override
+  Move pickMove(AgentView view) {
+    var initialPosition = view.getPositions(PieceType.king).first;
+    var targetPosition = view.closestOpponent(initialPosition, PieceType.king);
+    if (targetPosition == null) {
+      return _findRandomMove(view.legalMoves);
+    }
+    return findMoveByDistanceToTarget(view, targetPosition,
+        (double currentDistance, double bestDistance) {
+      return currentDistance > bestDistance;
+    });
+  }
+}
+
+class Opportunist extends Agent {
+  @override
+  String get name => 'Opportunist';
+
+  @override
+  Color get color => Colors.black;
+
+  @override
+  Move pickMove(AgentView view) {
+    var initialPosition = view.getPositions(PieceType.king).first;
+    var targetPosition = view.closestOpponent(initialPosition, PieceType.king);
+    if (targetPosition == null) {
+      return _findRandomMove(view.legalMoves);
+    }
     for (var move in view.legalMoves) {
-      var currentDistance =
-          move.finalPosition.deltaTo(targetPosition).magnitude;
-      if (bestDistance == null || isBetter(currentDistance, bestDistance)) {
-        bestDistance = currentDistance;
-        bestMove = move;
+      if (move.finalPosition == targetPosition) {
+        return move;
       }
     }
-    return bestMove!;
-  }
-}
-
-class Seeker extends DistanceEvaluatorAgent {
-  @override
-  bool isBetter(double currentDistance, double bestDistance) {
-    return currentDistance < bestDistance;
-  }
-}
-
-class Runner extends DistanceEvaluatorAgent {
-  @override
-  bool isBetter(double currentDistance, double bestDistance) {
-    return currentDistance > bestDistance;
+    return findMoveByDistanceToTarget(view, targetPosition,
+        (double currentDistance, double bestDistance) {
+      return currentDistance > bestDistance;
+    });
   }
 }
 
 class FirstMover extends Agent {
+  @override
+  String get name => 'FirstMover';
+
+  @override
+  Color get color => Colors.teal;
+
   @override
   Move pickMove(AgentView view) {
     return view.legalMoves.first;
   }
 }
 
-class RandomMover extends Agent {
-  @override
-  Move pickMove(AgentView view) {
-    var rng = Random();
-    final choices = view.legalMoves.toList();
-    return choices[rng.nextInt(choices.length)];
-  }
-}
-
 class IllegalMove {
   final String reason;
   const IllegalMove(this.reason);
+}
+
+Move _findRandomMove(Iterable<Move> legalMoves) {
+  var rng = Random();
+  var choices = legalMoves.toList();
+  return choices[rng.nextInt(choices.length)];
+}
+
+class RandomMover extends Agent {
+  @override
+  String get name => 'RandomMover';
+
+  @override
+  Color get color => Colors.lime;
+
+  @override
+  Move pickMove(AgentView view) {
+    return _findRandomMove(view.legalMoves);
+  }
 }
 
 class Fixate extends Agent {
@@ -147,9 +222,15 @@ class Fixate extends Agent {
   }
 
   @override
+  String get name => 'Fixate';
+
+  @override
+  Color get color => Colors.brown;
+
+  @override
   Move pickMove(AgentView view) {
     var legalMoves = view.legalMoves.toList();
-    var move = getMatchingFavorite(legalMoves) ?? getRandom(legalMoves);
+    final move = getMatchingFavorite(legalMoves) ?? _findRandomMove(legalMoves);
     favorite = move.delta;
     return move;
   }

a2a2fa8e7a3cc2544e3f8fea454bffc2091cb81a
CLS11 <crystall.101600@gmail.com>
2024-11-06 20:07:41 +0530

    Made player an abstract class.

--- lib/player.dart
+++ lib/player.dart
@@ -1,9 +1,20 @@
+import 'package:chess/pieces.dart';
 import 'package:flutter/material.dart';
 
-class Player {
-  const Player(this.name, this.color);
-  final String name;
-  final Color color;
+abstract class Player {
+  const Player();
+  String get name;
+  Color get color;
+
+  void paint(Canvas canvas, Rect rect, PieceType type) {
+    final paint = Paint();
+    paint.style = PaintingStyle.fill;
+    paint.color = color;
+    switch (type) {
+      case PieceType.king:
+        canvas.drawOval(rect, paint);
+    }
+  }
 
   @override
   String toString() => 'Player[$name]';

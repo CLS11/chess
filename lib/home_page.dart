da31ef5556060c8af20d96a1919efb44ffa496be
CLS11 <crystall.101600@gmail.com>
2024-11-06 21:18:06 +0530

    Maintained a record of Game.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:chess/board.dart';
 import 'package:chess/game_controller.dart';
+import 'package:chess/player.dart';
 import 'package:flutter/material.dart';
 
 class BattlePage extends StatefulWidget {
@@ -19,7 +21,7 @@ class _BattlePageState extends State<BattlePage> {
 
   @override
   void dispose() {
-    timer?.cancel;
+    timer?.cancel(); // Fix: add parentheses to cancel the timer correctly
     super.dispose();
   }
 
@@ -40,7 +42,7 @@ class _BattlePageState extends State<BattlePage> {
       gameState = gameController.getRandomInitialGameState();
     });
 
-    timer = Timer.periodic(const Duration(milliseconds: 1), _handleTimer);
+    timer = Timer.periodic(const Duration(milliseconds: 5), _handleTimer);
   }
 
   void _stopBattle() {
@@ -52,7 +54,7 @@ class _BattlePageState extends State<BattlePage> {
 
   void nextTurn() {
     setState(() {
-      gameState = gameController.takeTurn(gameState!);
+      gameState = gameController.takeTurn(gameState); // Remove nullable safety operator
     });
   }
 
@@ -85,9 +87,42 @@ class _BattlePageState extends State<BattlePage> {
   }
 }
 
+const double kValue = 16.0;
+const double initialRating = 500.0;
+
 class GameHistory {
   final Map<String, double> wins = <String, double>{};
   int gameCount = 0;
+
+  final Map<String, double> rating = <String, double>{};
+
+  double expectedScore(double currentRating, double opponentRating) {
+    var exponent = (opponentRating - currentRating) / 400.0;
+    return 1.0 / (1.0 + pow(10.0, exponent));
+  }
+
+  double pointsToTransfer(double score, double expectedScore) {
+    return kValue * (score - expectedScore);
+  }
+
+  double currentRatingForName(String name) {
+    return rating[name] ?? initialRating;
+  }
+
+  double currentRating(Player player) => currentRatingForName(player.name);
+
+  void adjustRating(Player player, double delta) {
+    rating[player.name] = currentRating(player) + delta;
+  }
+
+  void updateRating(Player winner, Player loser, double score) {
+    var winnerRating = currentRating(winner);
+    var loserRating = currentRating(loser);
+    var stake =
+        pointsToTransfer(score, expectedScore(winnerRating, loserRating));
+    adjustRating(winner, stake);
+    adjustRating(loser, -stake);
+  }
 
   void recordGame(GameState gameState) {
     var pointsPerPlayer = 1.0 / gameState.players.length;
@@ -95,6 +130,21 @@ class GameHistory {
       var name = player.name;
       wins[name] = (wins[name] ?? 0.0) + pointsPerPlayer;
       gameCount += 1;
+    }
+    for (var i = 0; i < gameState.deadPlayers.length - 1; i++) {
+      var winner = gameState.deadPlayers[i + 1];
+      var loser = gameState.deadPlayers[i];
+      updateRating(winner, loser, 1.0);
+    }
+    var alivePlayers = List<Player>.from(gameState.players);
+    alivePlayers.shuffle();
+    if (gameState.deadPlayers.isNotEmpty) {
+      updateRating(alivePlayers.first, gameState.deadPlayers.last, 1.0);
+    }
+    for (var i = 0; i < alivePlayers.length; i++) {
+      for (var j = i + 1; j < alivePlayers.length; j++) {
+        updateRating(alivePlayers[i], alivePlayers[j], 0.5);
+      }
     }
   }
 }

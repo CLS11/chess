4e08b2fb293219189712b7076dbf9016ae5ef62d
CLS11 <crystall.101600@gmail.com>
2024-11-06 20:00:45 +0530

    Amended the list of Agent.

--- lib/game_controller.dart
+++ lib/game_controller.dart
@@ -1,6 +1,8 @@
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:chess/agents.dart' as agents;
+import 'package:chess/agents.dart';
 import 'package:chess/board.dart';
 import 'package:chess/pieces.dart';
 import 'package:chess/player.dart';
@@ -8,35 +10,22 @@ import 'package:chess/positions.dart';
 import 'package:flutter/material.dart';
 
 class GameController {
-  factory GameController.demo() {
-    var controller = GameController();
-    controller.addPlayerWithAgent(
-      const Player('Random', Colors.black),
-      agents.RandomMover(),
-    );
-    controller.addPlayerWithAgent(
-        const Player("Seeker", Colors.blue), agents.Seeker());
+  GameController.withAgents(this._agents);
 
-    controller.addPlayerWithAgent(
-        const Player("Runner", Colors.white), agents.Runner());
-    controller.addPlayerWithAgent(
-        const Player("FirstMover", Colors.teal), agents.FirstMover());
-    controller.addPlayerWithAgent(
-        const Player("Fixate", Colors.lime), agents.Fixate());
-    return controller;
+  factory GameController.withRandomAgents(int numberOfPlayers) {
+    var rng = Random();
+    return GameController.withAgents(List<Agent>.generate(numberOfPlayers,
+        (index) => agents.all[rng.nextInt(agents.all.length)]()));
   }
-  final Map<Player, agents.Agent> _agents = <Player, agents.Agent>{};
 
-  GameController();
+  final List<Agent> _agents;
 
-  void addPlayerWithAgent(Player player, agents.Agent agent) {
-    _agents[player] = agent;
-  }
+  GameController() : _agents = <Agent>[];
 
   GameState getRandomInitialGameState() {
     var board = Board.empty();
-    var players = _agents.keys.toList();
-    for (var player in players) {
+
+    for (var player in _agents) {
       Positions position;
       do {
         position = Positions.random();
@@ -44,13 +33,13 @@ class GameController {
 
       board = board.placeAt(position, Pieces(PieceType.king, player));
     }
-    return GameState(board, players);
+    return GameState(board, List<Player>.from(_agents));
   }
 
   GameState takeTurn(GameState gameState) {
     var activePlayer = gameState.activePlayer;
     var view = agents.AgentView(gameState, activePlayer);
-    var activeAgent = _agents[activePlayer]!;
+    var activeAgent = activePlayer as Agent;
     return gameState.move(activeAgent.pickMove(view));
   }
 }
